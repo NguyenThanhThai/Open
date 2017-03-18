092b27645af31c3daa54c6c8535d5ef7c2fca3d3
Aditya-Surabi <adityasurabi@gmail.com>
2017-03-18 15:07:39 -0400

    Added local file to url conversion function, fixed stupid callback things. thank you Lavan.

--- Open/index.js
+++ Open/index.js
@@ -1,6 +1,17 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var request = new XMLHttpRequest();
 
+var cloudinary = require('cloudinary');
+
+
+var url;
+
+cloudinary.config({
+  cloud_name: 'dqickes7k',
+  api_key: '455556278463497',
+  api_secret: 'd_VQbBuCTrua5LnKnlxPP6g0C7k'
+});
+
 // request.open('POST','https://api.kairos.com/enroll');
 //
 // request.setRequestHeader('Content-Type', 'application/json')
@@ -24,33 +35,58 @@ var request = new XMLHttpRequest();
 // request.send(JSON.stringify(body));
 
 
+function faceRecog(u){
+  var status;
+  var confindence;
 
-var status;
-var confindence;
+  request.open('POST','https://api.kairos.com/recognize');
 
-request.open('POST','https://api.kairos.com/recognize');
+  request.setRequestHeader('Content-Type', 'application/json')
+  request.setRequestHeader('app_id', 'd63ffed3');
+  request.setRequestHeader('app_key', 'e7e0da0ec38c0e67a52894e2698b99d8');
 
-request.setRequestHeader('Content-Type', 'application/json')
-request.setRequestHeader('app_id', 'd63ffed3');
-request.setRequestHeader('app_key', 'e7e0da0ec38c0e67a52894e2698b99d8');
+  request.onreadystatechange = function () {
+    if (this.readyState ===4){
+      console.log('Status', this.status);
+      console.log('Headers', this.getAllResponseHeaders);
+      console.log(this.responseText);
+      responseJson = JSON.parse(this.responseText);
+      confindence = responseJson.images[0].transaction.confidence;
+      status = responseJson.images[0].transaction.status
+      console.log("status: "+status);
+      console.log("confindence" + confindence);
+      // result = this.responseText[1].images;
+      // console.log('This is the result:' +result);
 
-request.onreadystatechange = function () {
-  if (this.readyState ===4){
-    console.log('Status', this.status);
-    console.log('Headers', this.getAllResponseHeaders);
-    console.log(this.responseText);
-    status = JSON.parse(this.responseText);
-    confindence = status.images[0].transaction.confidence;
-    console.log("status: "+status.images[0].transaction.status);
-    console.log("confindence" + confindence);
-    // result = this.responseText[1].images;
-    // console.log('This is the result:' +result);
-  }
-};
+    if(status == "success"){
+      isMatch();
+    }
+    else{
+      isNotMatch();
+    }
+    }
+  };
 
-var body = {
-  'image': 'kim-jong-un-in-suit.jpg',
-  'gallery_name': 'mygallery'
-};
+  var body = {
+    'image': u,
+    'gallery_name': 'mygallery'
+  };
 
-request.send(JSON.stringify(body));
+  request.send(JSON.stringify(body));
+}
+
+
+cloudinary.uploader.upload("batman.jpg", function(result) {
+  var u = result.url;
+  console.log("The URL is:" +u);
+  faceRecog(u);
+
+});
+
+function isMatch() {
+
+}
+
+function isNotMatch() {
+
+}
